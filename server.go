f335d2d5e9e0e02f7949a9690c383c6d1649a798
sshehrozali <sshehroza13@gmail.com>
2024-09-09 15:42:01 +0500

    written basic logic

---
+++ server.go
@@ -0,0 +1,90 @@
+package filedownloader
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"net/http"
+)
+
+func main() {
+
+	// Perform HTTP GET request
+	// Ensure status is 200OK and no error
+	// Check file MIME type
+	// Decode body in bytes -> make sure no errors
+	// Transform, write the file locally
+
+	response, err := http.Get("url")
+
+	if err != nil {
+		fmt.Println("Error while performing GET request")
+	}
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Request failed with HTTP status: ", response.StatusCode)
+	}
+
+	mimeType := response.Header.Get(("Content-Type"))
+	fmt.Println("Downloaded content type: ", mimeType)
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("Error while decoding response body")
+	}
+
+	commonMIMEtypes := map[string]string{
+		// Text types
+		"text/plain":        ".txt",
+        "text/html":         ".html",
+        "text/css":          ".css",
+        "text/javascript":   ".js",
+
+        // Image types
+        "image/jpeg":        ".jpeg",
+        "image/png":         ".png",
+        "image/gif":         ".gif",
+        "image/svg+xml":     ".svg",
+
+        // Audio types
+        "audio/mpeg":        ".mp3",
+        "audio/wav":         ".wav",
+        "audio/ogg":         ".ogg",
+
+        // Video types
+        "video/mp4":         ".mp4",
+        "video/webm":        ".webm",
+        "video/ogg":         ".ogv",
+
+        // Application types
+        "application/json": ".json",
+        "application/xml":  ".xml",
+        "application/pdf":  ".pdf",
+        "application/zip":  ".zip",
+        "application/octet-stream": ".bin",
+        "application/x-www-form-urlencoded": ".url",
+        
+        // Multipart types
+        "multipart/form-data": ".multpart",
+		
+	}
+
+	fileExtension := commonMIMEtypes[mimeType]
+	file, err := os.Create("downloaded_file" + fileExtension)
+	defer file.Close()
+	
+	bytesWritten, err := file.Write(body)
+
+	if (err != nil) {
+		fmt.Println("Error while writing data in file...")
+	}
+
+	if (bytesWritten <= 0) {
+		fmt.Println("No bytes written in file...")
+	}
+
+	fmt.Println("No of bytes copied: " + string(bytesWritten))
+	fmt.Println("File downloaded successfully and saved to current local directory.")
+
+}
