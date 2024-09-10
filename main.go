e07fcc19590a2e87500aea8f2d8c947c9cbc85ab
sshehrozali <sshehroza13@gmail.com>
2024-09-10 16:09:35 +0500

    refactored into functions

--- main.go
+++ main.go
@@ -71,6 +71,20 @@ type DownloadResult struct {
     MimeType string
 }
 
+func promptInput() string {
+	var url string
+
+	fmt.Print("Enter download URL: ")
+	fmt.Scanln(&url)
+
+	if url == "" {
+		fmt.Println("\nProvided download URL is empty.")
+		os.Exit(1)
+	}
+
+	return url
+}
+
 func downloadFile(url string) DownloadResult {
 	fmt.Println("\nFetching data from the server...")
 
@@ -110,27 +124,8 @@ func downloadFile(url string) DownloadResult {
 	}
 }
 
-func main() {
-
-	// Perform HTTP GET request
-	// Ensure status is 200OK and no error
-	// Check file MIME type
-	// Decode body in bytes -> make sure no errors
-	// Transform, write the file locally
-
-	var url string
-
-	fmt.Print("Enter download URL: ")
-	fmt.Scanln(&url)
-
-	if url == "" {
-		fmt.Println("\nProvided download URL is empty.")
-		os.Exit(1)
-	}
-
-	downloadResult := downloadFile(url)
-
-	fileName := "downloaded_file" + fileExtensionRetriever(downloadResult.MimeType)
+func saveLocally(data DownloadResult) {
+	fileName := "downloaded_file" + fileExtensionRetriever(data.MimeType)
 	file, fileErr := os.Create(fileName)
 	if fileErr != nil {
 		fmt.Printf("Error opening %s\n", fileName)
@@ -139,7 +134,7 @@ func main() {
 	}
 	defer file.Close()	// Close the file automatically once I/O operations are finished 
 
-	bytesWritten, err := file.Write(downloadResult.Body)
+	bytesWritten, err := file.Write(data.Body)
 
 	if err != nil {
 		fmt.Println("Error copying bytes locally.")
@@ -154,4 +149,15 @@ func main() {
 	fmt.Printf("Downloaded file size: %d bytes\n", bytesWritten)
 	fmt.Printf("File downloaded successfully. Check your current local directory %s\n", fileName)
 	os.Exit(0)
+}
+
+
+
+
+func main() {
+
+	url := promptInput()
+	downloadResult := downloadFile(url)
+	saveLocally(downloadResult)
+
 }
