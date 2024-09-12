1918830419788df3aad1fab8e65ce247a05f2f97
sshehrozali <sshehroza13@gmail.com>
2024-09-12 14:19:45 +0500

    Updated go.mod, TestPromptInput() passed

--- main_test.go
+++ main_test.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-    "io/ioutil"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"os"
+	"testing"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPromptInput(t *testing.T) {
-	// xyz logic
+	mockUrl := "www.example.com/download\n"
+
+    originalStdin := os.Stdin
+
+    // Open pipe for read-write
+    r, w, err := os.Pipe()
+    if err != nil {
+        return
+    }
+
+    w.Write([]byte(mockUrl))
+    w.Close()
+
+    // Replace stdin with read-end
+    os.Stdin = r
+
+    actualUrl := promptInput()
+
+    assert.Equal(t, "www.example.com/download", actualUrl, "Expected equals actual")
+
+    // Restore original OS Stdin
+    os.Stdin = originalStdin
 }
 
 func TestDownloadFile(t *testing.T) {
