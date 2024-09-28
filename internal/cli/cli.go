a0e021946298a646778811ad9ac9741ea581fb32
Nehad Shakoor <98046541+Nehadsys@users.noreply.github.com>
2024-09-28 22:39:07 +0500

    URL Validation

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"errors"
 	"flag"
+	"strings"
 )
 
 func PromptInput() (string, error) {
@@ -11,26 +12,13 @@ func PromptInput() (string, error) {
 	// output := flag.String("o", "output_file", "Output file path (default: output_file)")
 	flag.Parse()
 
-<<<<<<< HEAD
-	// Error Handling
-
-	if url == "" {
-		return "", errors.New("url can't be empty")
-	}
-
-	if len(url) < 8 || url[:8] != "https://" {
-		return "", errors.New("The URL must contain https://")
-	}
-
-	return url, nil
-
-=======
 	if *url == "" {
 		return "", errors.New("please provide a URL using the -url flag")
 	}
 
-	return *url, nil
->>>>>>> 7a3fa8c967bd63bd6281ddef57f1b6a33612a015
-}
+	if !strings.HasPrefix(*url, "https://") {
+		return "", errors.New("The URL must contain https://")
+	}
 
-// Sample URL -> https://filesampleshub.com/format/image/jpg
+	return *url, nil
+}
